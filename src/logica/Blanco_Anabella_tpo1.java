f8c926776fceb70eb2957207745e76b3289d1224
AnabellaEstrella <anabella.blanco.97@gmail.com>
2021-09-23 16:13:22 -0300

    Primer commit - Subo archivos que conforman el TP

---
+++ src/logica/Blanco_Anabella_tpo1.java
@@ -0,0 +1,17 @@
+package logica;
+
+import igu.Principal;
+
+/**
+ *
+ * @author AnabellaBlanco
+ */
+public class Blanco_Anabella_tpo1 {
+
+    public static void main(String[] args) {
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
